61b2f260b84c9987c91e13b2c49c375038e9e921
kmthein <kmthein96.mm@gmail.com>
2023-11-23 23:56:43 +0630

    d-65 more validation & timestamp

--- controllers/post.js
+++ controllers/post.js
@@ -2,10 +2,21 @@ const posts = [];
 
 const Post = require("../models/post");
 
+const { validationResult } = require("express-validator");
+
+const formatISO9075 = require('date-fns/formatISO9075')
+
 exports.createPost = (req, res) => {
   const { title, description, photo } = req.body;
   // const userId = req.session.userInfo._id;
-
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("addPost", {
+      title: "Add New Post",
+      errorMsg: errors.array()[0].msg,
+      oldFormData: { title, description, photo },
+    });
+  }
   Post.create({ title, description, imgUrl: photo, userId: req.user._id })
     .then((result) => {
       res.redirect("/");
@@ -18,14 +29,18 @@ exports.createPost = (req, res) => {
 exports.renderCreatePage = (req, res) => {
   // res.sendFile(path.join(__dirname, "..", "views", "addPost.html"));
 
-  res.render("addPost", { title: "Add New Post" });
+  res.render("addPost", {
+    title: "Add New Post",
+    errorMsg: "",
+    oldFormData: { title: "", description: "", photo: "" },
+  });
 };
 
 exports.renderHomePage = (req, res) => {
   // res.sendFile(path.join(__dirname, "..", "views", "homepage.html"));
   // const cookie = req.get("Cookie").split("=")[1].trim() === "true";
   let loginSuccessMsg = req.flash("loginSuccess");
-  if(loginSuccessMsg.length > 0) {
+  if (loginSuccessMsg.length > 0) {
     loginSuccessMsg = loginSuccessMsg[0];
   } else {
     loginSuccessMsg = null;
@@ -41,7 +56,7 @@ exports.renderHomePage = (req, res) => {
         isLogin: req.session.isLogin ? true : false,
         loginSuccessMsg,
         currentUser: req.session.userInfo ? req.session.userInfo.email : "",
-        csrfToken: req.csrfToken()
+        csrfToken: req.csrfToken(),
       });
     })
     .catch((err) => {
@@ -53,7 +68,7 @@ exports.getEditPost = (req, res) => {
   const postId = req.params.postId;
   Post.findById(postId)
     .then((post) => {
-      res.render("editPost", { title: post.title, post });
+      res.render("editPost", { title: post.title, post, errorMsg: "", isValidationFail: false });
     })
     .catch((err) => {
       console.log(err);
@@ -62,9 +77,24 @@ exports.getEditPost = (req, res) => {
 
 exports.getPost = (req, res) => {
   const postId = req.params.postId;
-  Post.findById(postId)
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("addPost", {
+      title: "Add New Post",
+      errorMsg: errors.array()[0].msg,
+      oldFormData: { title, description, photo },
+    });
+  }
+  Post.findById(postId).populate("userId", "email")
     .then((post) => {
-      res.render("details", { title: "Post Details", post, currentLoginUserId: req.session.userInfo ? req.session.userInfo._id : "" });
+      res.render("details", {
+        title: "Post Details",
+        post,
+        date: post.createdAt ? formatISO9075(post.createdAt, {representation: "date"}) : undefined,
+        currentLoginUserId: req.session.userInfo
+          ? req.session.userInfo._id
+          : "",
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -76,8 +106,18 @@ exports.editPost = (req, res) => {
 
   Post.findById(postId)
     .then((post) => {
-      if(post.userId.toString() != req.user._id.toString()) {
+      if (post.userId.toString() != req.user._id.toString()) {
         return res.redirect("/");
+      }
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).render("editPost", {
+          title: post.title,
+          errorMsg: errors.array()[0].msg,
+          post,
+          oldFormData: { title, description, photo },
+          isValidationFail: true
+        });
       }
       post.title = title;
       post.description = description;
@@ -95,7 +135,7 @@ exports.editPost = (req, res) => {
 exports.deletePost = (req, res) => {
   const { postId } = req.params;
 
-  Post.deleteOne({_id: postId, userId: req.user._id})
+  Post.deleteOne({ _id: postId, userId: req.user._id })
     .then(() => {
       console.log("Post Deleted");
       res.redirect("/");
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const path = require("path");
-
+const { body } = require("express-validator");
 const router = express.Router();
 const postController = require("../controllers/post")
 
 // /admin/create-post
 router.get("/create-post", postController.renderCreatePage);
 
-router.post("/", postController.createPost);
+router.post("/", [body("title").isLength({ min: 10 }).withMessage("Title must be at least 10 characters."), body("photo").isURL().withMessage("Photo must be URL link."), body("description").isLength({ min: 30 }).withMessage("Description must be at least 30 characters.")] ,postController.createPost);
 
 router.get("/edit/:postId", postController.getEditPost);
 
-router.post("/edit-post", postController.editPost);
+router.post("/edit-post", [body("title").isLength({ min: 10 }).withMessage("Title must be at least 10 characters."), body("photo").isURL().withMessage("Photo must be URL link."), body("description").isLength({ min: 30 }).withMessage("Description must be at least 30 characters.")], postController.editPost);
 
 router.post("/delete-post/:postId", postController.deletePost);
 
